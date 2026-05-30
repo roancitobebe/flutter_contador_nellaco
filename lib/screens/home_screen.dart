d69f0471ae86512d134fd4002070a004eb845ee3
roancitobebe <roandmoreno123@gmail.com>
2026-05-30 18:46:40 -0400

    body listo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,9 +9,22 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+
+  int valor = 0 ;
   Widget build(BuildContext context) {
-    return  Scaffold(appBar: AppBar(
-  title: Text("BELLAQUIANDO"),
-), ) ;
+    return  Scaffold(appBar:
+     AppBar(
+  title: 
+  Text("BELLAQUIANDO"),
+  centerTitle: true,
+), 
+body: Center(
+  child:
+   Column(mainAxisAlignment: MainAxisAlignment.center,
+   children: [
+    const Text("gosalo"),
+    Text('$valor', style: const TextStyle(fontSize: 40)),]
+) ,)) ;
+
   }
 }
