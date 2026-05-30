9b1422dd1faa2f37a417eeba061f67ae27c0b1d2
roancitobebe <roandmoreno123@gmail.com>
2026-05-30 15:30:03 -0400

    primeros pasos

---
+++ android/app/src/main/kotlin/com/example/flutter_contador_nellaco/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_contador_nellaco
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main () {runApp(const Myapp())} ;
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeScreens(),);
+  }
+}
